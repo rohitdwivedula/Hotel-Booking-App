b11f0789479818a700c1a2f2f0e8eed4a24e2f4d
rohitdwivedula <rohitdwivedula@gmail.com>
2018-11-11 16:37:39 +0530

    Updated Databases

--- Databases/booking_info.sql
+++ Databases/booking_info.sql
@@ -1,4 +1,12 @@
 CREATE TABLE booking_info(
-booking_id INT NOT NULL,
-room_id 
+Booking_ID INT NOT NULL,
+username VARCHAR(255) NOT NULL, 
+Room_ID INTEGER NOT NULL,
+Hotel_ID INTEGER NOT NULL,
+Date_In DATE NOT NULL,
+Date_Out DATE NOT NULL,
+Valid INT NOT NULL,
+PRIMARY KEY (Booking_ID),
+FOREIGN KEY (username) REFERENCES user_info(username),
+FOREIGN KEY (Room_ID) REFERENCES room_info(Room_ID)
 );
